2b15018cfc1ff1dda50fb73246c23ec02d5aa275
mgpdm <167898942+mgpdm@users.noreply.github.com>
2024-04-29 19:31:45 +0000

    Exercicio 2

--- exercicios/1-CRUD/2-alterar-tabelas.sql
+++ exercicios/1-CRUD/2-alterar-tabelas.sql
@@ -1,3 +1,7 @@
--- Na tabela salario_classe altere o atributo "nivel" para que seu tipo de dado seja varchar, com tamanho máximo de 50 caracteres
-
+-- Na tabela salario_classe altere o atributo "nivel" para que seu 
+--tipo de dado seja varchar, com tamanho máximo de 50 caracteres
+ALTER TABLE salario_classe
+ADD salario varchar(50);
 -- Na tabela employees, adicione uma coluna para receber o id_salario
+alter TABLE employees
+ADD id_salario INTEGER;
